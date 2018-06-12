746f21454c5772d2e37630877b4e31aa0f1c5563
xiaozao <xiaozao2008@msn.cn>
2018-06-12 16:51:08 +0800

    add Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+#  WolfSegment
+
+### 一种替代UISegmentedControl的滑动方式，模仿谷歌产品/网易新闻中使用的分段控制的风格。
+
+
+## 使用简单 欢迎随时需求完善
+
+
+        let wolfSegment = WolfSegmentBar.init(frame: CGRect.init(x: 0, y: 100, width: view.frame.width, height: 40),
+                                              titles: ["Apple Store", "头条", "世界杯", "测试长文字", "新时代", "娱乐", "体育", "要闻", "段子", "微资讯", "本地", "科技", "Apple Store"],
+                                              config: WolfButtonConfig.default())
+        view.addSubview(wolfSegment)
+        
+        wolfSegment.selectIndex = { index in
+            print(index)
+        }
+        
+
+
--- WolfSegMent/ViewController.swift
+++ WolfSegMent/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let segments = WolfSegmentBar.init(frame: CGRect.init(x: 0, y: 100, width: view.frame.width, height: 40),
+        let wolfSegment = WolfSegmentBar.init(frame: CGRect.init(x: 0, y: 100, width: view.frame.width, height: 40),
                                            titles: ["Apple Store", "头条", "世界杯", "测试长文字", "新时代", "娱乐", "体育", "要闻", "段子", "微资讯", "本地", "科技", "Apple Store"],
                                            config: WolfButtonConfig.default())
-        view.addSubview(segments)
-        segments.selectIndex = { index in
+        view.addSubview(wolfSegment)
+        wolfSegment.selectIndex = { index in
             print(index)
         }
         
